7d5a07690d3245de66463320a591c9bf1b6cc2b4
thomas <thomas.chu@techdesign.com>
2022-07-19 22:26:29 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import vuetify from "./plugins/vuetify";
-//
 import "@/assets/Style/Global.css";
+
+import App from "./App.vue";
+import Vue from "vue";
+import router from "./router";
+
+//
+
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  vuetify,
-  render: (h) => h(App),
+    router,
+    render: (h) => h(App),
 }).$mount("#app");
